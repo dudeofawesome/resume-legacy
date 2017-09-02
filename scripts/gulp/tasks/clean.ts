94bb117f0f634d7844a6eafa67a682821f7dd632
Louis Orleans <louis@orleans.io>
2017-09-01 22:59:08 -0700

    ✨ Building manifest

--- scripts/gulp/tasks/clean.ts
+++ scripts/gulp/tasks/clean.ts
@@ -1,8 +1,9 @@
 import * as Gulp from 'gulp';
 import * as Del from 'del';
 
-Gulp.task(`clean`, [`clean:js`, `clean:html`, `clean:css`, `clean:assets`]);
+Gulp.task(`clean`, [`clean:js`, `clean:html`, `clean:css`, `clean:assets`, `clean:manifest`]);
 Gulp.task(`clean:js`, () => Del([`build/**/*.js`, `build/**/*.d.ts`]));
 Gulp.task(`clean:html`, () => Del([`build/**/*.html`]));
 Gulp.task(`clean:css`, () => Del([`build/**/*.css`, `build/**/*.sass`]));
 Gulp.task(`clean:assets`, () => Del([`build/assets`]));
+Gulp.task(`clean:manifest`, () => Del([`build/manifest.json`]));
