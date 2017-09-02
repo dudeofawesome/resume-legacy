caef0612ba3d26f5a0116754eb49d8ad916a29ef
Louis Orleans <louis@orleans.io>
2017-09-02 01:35:03 -0700

    🐛 Watching fragments

--- scripts/gulp/tasks/watch.ts
+++ scripts/gulp/tasks/watch.ts
@@ -4,6 +4,6 @@ import { SRC } from '../constants';
 
 Gulp.task(`watch`, [`watch:html`, `watch:sass`, `watch:assets`, `watch:typescript`]);
 Gulp.task(`watch:sass`, () => Gulp.watch(SRC.SASS, [`build:sass`]));
-Gulp.task(`watch:html`, () => Gulp.watch([SRC.HTML, SRC.DATA], [`build:html`]));
+Gulp.task(`watch:html`, () => Gulp.watch([SRC.HTML, SRC.HTML_FRAGMENTS, SRC.DATA], [`build:html`]));
 Gulp.task(`watch:assets`, () => Gulp.watch(SRC.ASSETS, [`build:assets`]));
 Gulp.task(`watch:typescript`, () => Gulp.watch(SRC.TYPESCRIPT, [`build:typescript`]));
