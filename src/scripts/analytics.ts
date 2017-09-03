885b71d5f2d9955d708e697d8e360578c9ba79b3
Louis Orleans <louis@orleans.io>
2017-09-02 21:18:26 -0700

    🐛 Fixing outbound link tracking

--- src/scripts/analytics.ts
+++ src/scripts/analytics.ts
@@ -4,16 +4,35 @@ if (window.location.hostname !== `localhost`) {
 
 ga('send', 'pageview');
 
+const continueNavigation = (url: string): () => void => {
+  return () => window.location.href = url;
+}
+
 const listener = (ev: MouseEvent) => {
   const anchor: HTMLAnchorElement = ev.srcElement as HTMLAnchorElement;
-  ga('send', {
-    hitType: 'event',
-    eventCategory: 'Click Link',
-    eventAction: anchor.hasAttribute('name') ? anchor.getAttribute('name') : anchor.innerHTML,
-    eventLabel: anchor.href
+
+  // Send outbound link click event to GA
+  ga('send', 'event', {
+    eventCategory: 'Outbound Link',
+    eventAction: ev.type,
+    eventLabel: anchor.href,
+    eventValue: +anchor.getAttribute('value') || 10,
+    // Use a beacon, if available
+    transport: navigator.sendBeacon != null ? 'beacon' : 'auto',
+    hitCallback: continueNavigation(anchor.href)
   });
+
+  /**
+   * Force page navigation in case we fail to reach GA
+   * https://developers.google.com/analytics/devguides/collection/analyticsjs/sending-hits#knowing_when_the_hit_has_been_sent
+   */
+  setTimeout(continueNavigation(anchor.href), 1000);
+
+  ev.preventDefault();
+  return false;
 };
 
+// Find all anchor tags and add click listeners to them
 const anchors = document.getElementsByTagName('a');
 for (let i = 0; i < anchors.length; i++) {
   anchors[i].addEventListener('click', listener);
