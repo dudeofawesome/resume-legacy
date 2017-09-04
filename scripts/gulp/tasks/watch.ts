9750d31f319baf89b984a189e1618b76bb64179e
Louis Orleans <louis@orleans.io>
2017-09-04 14:36:05 -0700

    ✨💬 Adding link to web resume in print view

--- scripts/gulp/tasks/watch.ts
+++ scripts/gulp/tasks/watch.ts
@@ -4,7 +4,7 @@ import { SRC } from '../constants';
 
 Gulp.task(`watch`, [`watch:html`, `watch:sass`, `watch:manifest`, `watch:assets`, `watch:typescript`]);
 Gulp.task(`watch:sass`, () => Gulp.watch(SRC.SASS, [`build:sass`]));
-Gulp.task(`watch:html`, () => Gulp.watch([SRC.HTML, SRC.HTML_FRAGMENTS, SRC.DATA], [`build:html`]));
+Gulp.task(`watch:html`, () => Gulp.watch([SRC.HTML, SRC.HTML_FRAGMENTS, SRC.DATA, SRC.PACKAGE], [`build:html`]));
 Gulp.task(`watch:manifest`, () => Gulp.watch([SRC.MANIFEST], [`build:manifest`]));
 Gulp.task(`watch:assets`, () => Gulp.watch(SRC.ASSETS, [`build:assets`]));
 Gulp.task(`watch:typescript`, () => Gulp.watch(SRC.TYPESCRIPT, [`build:typescript`]));
