6001d782548fa6bcf58610872139895b702a0148
Louis Orleans <louis@orleans.io>
2018-03-24 19:33:53 -0700

    🚧👷✨ start Chrome manually

--- scripts/cd/render-pdf.sh
+++ scripts/cd/render-pdf.sh
@@ -4,7 +4,7 @@ pwd;
 ls build;
 echo "[BREAK]";
 http-server -p 8080 build &
-# google-chrome-stable --headless --disable-gpu --remote-debugging-port=9222 http://localhost &
+google-chrome-stable --headless --disable-gpu --remote-debugging-port=9222 "http://localhost:8080" &
 chrome-headless-render-pdf --url "http://localhost:8080" --pdf "build/Louis Orleans' Résumé.pdf" --no-margins;
 kill $!
 ls build;
