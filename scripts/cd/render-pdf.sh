6079d10e215eb07d8e8c2fa695f0b621bc14a306
Louis Orleans <louis@orleans.io>
2018-03-24 19:22:40 -0700

    🚧👷✨ render pdf after build

--- scripts/cd/render-pdf.sh
+++ scripts/cd/render-pdf.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 pwd;
-ls bulid;
+ls build;
 echo "[BREAK]";
-http-server -p 8080 build &;
+http-server -p 8080 build &
 # google-chrome-stable --headless --disable-gpu --remote-debugging-port=9222 http://localhost &
 chrome-headless-render-pdf --url "http://localhost:8080" --pdf "build/Louis Orleans' Résumé.pdf";
-kill $!;
+kill $!
 ls build;
 echo "[BREAK]";
