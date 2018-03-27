36185996bc494e183d374e96e192b0635fdac248
Louis Orleans <louis@orleans.io>
2018-03-26 18:44:31 -0700

    👷🐛 sleep after Chrome start

--- scripts/cd/render-pdf.sh
+++ scripts/cd/render-pdf.sh
@@ -7,6 +7,7 @@ http-server -p 8080 build &
 sleep 10;
 echo "[START HEADLESS CHROME]";
 google-chrome-stable --headless --disable-gpu --remote-debugging-port=9222 "http://localhost:8080" &
+sleep 60;
 echo "[CAPTURE WEBSITE PDF]";
 chrome-headless-render-pdf --url "http://localhost:8080" --pdf "build/Louis Orleans' Résumé.pdf" --no-margins;
 echo "[STOP HEADLESS CHROME]";
