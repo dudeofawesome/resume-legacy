d8759697f5dfa67165d4c58caf2f8bca326239af
Louis Orleans <louis@orleans.io>
2018-03-26 18:29:43 -0700

    👷🐛 sleep after web server start

--- scripts/cd/render-pdf.sh
+++ scripts/cd/render-pdf.sh
@@ -4,6 +4,7 @@ pwd;
 ls build;
 echo "[START HTTP SERVER]";
 http-server -p 8080 build &
+sleep 10;
 echo "[START HEADLESS CHROME]";
 google-chrome-stable --headless --disable-gpu --remote-debugging-port=9222 "http://localhost:8080" &
 echo "[CAPTURE WEBSITE PDF]";
