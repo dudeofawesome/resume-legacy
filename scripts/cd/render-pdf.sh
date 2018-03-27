3394615d3d4504995b5907fc82eaa93355579cd2
Louis Orleans <louis@orleans.io>
2018-03-26 18:16:09 -0700

    🚧👷🔊 add more logging to PDF render script

--- scripts/cd/render-pdf.sh
+++ scripts/cd/render-pdf.sh
@@ -2,10 +2,13 @@
 
 pwd;
 ls build;
-echo "[BREAK]";
+echo "[START HTTP SERVER]";
 http-server -p 8080 build &
+echo "[START HEADLESS CHROME]";
 google-chrome-stable --headless --disable-gpu --remote-debugging-port=9222 "http://localhost:8080" &
+echo "[CAPTURE WEBSITE PDF]";
 chrome-headless-render-pdf --url "http://localhost:8080" --pdf "build/Louis Orleans' Résumé.pdf" --no-margins;
+echo "[STOP HEADLESS CHROME]";
 kill $!
 ls build;
-echo "[BREAK]";
+echo "[DONE]";
