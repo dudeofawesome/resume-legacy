0c2a96591d4eb45380ff36c86329043fb3bd8991
Louis Orleans <louis@orleans.io>
2018-03-28 19:34:10 -0700

    👷🐛 stop using Chrome sandbox
    This should fix the Chrome SUID sandbox helper binary issue

--- scripts/cd/render-pdf.sh
+++ scripts/cd/render-pdf.sh
@@ -6,7 +6,7 @@ echo "[START HTTP SERVER]";
 http-server -p 8080 build &
 sleep 10;
 echo "[START HEADLESS CHROME]";
-google-chrome-stable --headless --disable-gpu --remote-debugging-port=9222 "http://localhost:8080" &
+google-chrome-stable --headless --disable-gpu --remote-debugging-port=9222 --no-sandbox "http://localhost:8080" &
 sleep 60;
 echo "[CAPTURE WEBSITE PDF]";
 chrome-headless-render-pdf --url "http://localhost:8080" --pdf "build/Louis Orleans' Résumé.pdf" --no-margins;
