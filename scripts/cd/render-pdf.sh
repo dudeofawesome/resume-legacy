255e1ef92bba7f361c64663a058f0f09efddf2bc
Louis Orleans <louis@orleans.io>
2018-03-31 17:02:06 -0700

    🐛✏️ fix Chrome binary path

--- scripts/cd/render-pdf.sh
+++ scripts/cd/render-pdf.sh
@@ -9,7 +9,7 @@ echo "[START HEADLESS CHROME]";
 google-chrome-stable --headless --disable-gpu --remote-debugging-port=9222 --no-sandbox "http://localhost:8080" &
 sleep 60;
 echo "[CAPTURE WEBSITE PDF]";
-chrome-headless-render-pdf --chrome-binary /usr/bin/google-chrome-stabl --url "http://localhost:8080" --pdf "build/Louis Orleans' Résumé.pdf" --no-margins;
+chrome-headless-render-pdf --chrome-binary /usr/bin/google-chrome-stable --url "http://localhost:8080" --pdf "build/Louis Orleans' Résumé.pdf" --no-margins;
 echo "[STOP HEADLESS CHROME]";
 kill $!
 echo "[STOP HTTP SERVER]";
